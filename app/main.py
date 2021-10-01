0e7f5557339bd1d328c1802f909aa5c6621ae369
Ali Raza <dejavu87@gmail.com>
2021-10-01 20:33:02 +0500

    adding persist files

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from classes.shortner import Shortener
 
@@ -12,3 +13,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
